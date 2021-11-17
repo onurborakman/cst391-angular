42ebd0bd6614b275f837c1709a6d2c78a92a8e3c
Onur Bora Akman <45215868+onurborakman@users.noreply.github.com>
2021-11-17 03:43:14 -0700

    Add files via upload

---
+++ noteapp/src/app/app-routing.module.ts
@@ -0,0 +1,46 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { CreateComponent } from './create/create.component';
+import { DeleteComponent } from './delete/delete.component';
+import { EditComponent } from './edit/edit.component';
+import { LoginComponent } from './login/login.component';
+import { LogoutComponent } from './logout/logout.component';
+import { NotesComponent } from './notes/notes.component';
+import { RegisterComponent } from './register/register.component';
+
+const routes: Routes = [
+  {
+    path: 'register',
+    component: RegisterComponent
+  },
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'logout',
+    component: LogoutComponent
+  },
+  {
+    path: 'notes',
+    component: NotesComponent
+  },
+  {
+    path: 'create',
+    component: CreateComponent
+  },
+  {
+    path: 'edit/:id',
+    component: EditComponent
+  },
+  {
+    path: 'delete/:id',
+    component: DeleteComponent
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes, {enableTracing: true})],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
